2d7ba67d2270a4f790120a0078865c18ede502c9
Savor d'Isavano <anohigisavay@gmail.com>
2016-05-31 00:26:50 +0800

    Initial commit

---
+++ Promofree.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Promofree
+// @namespace    https://github.com/TOSPIO/promofree
+// @version      0.1
+// @description  一起抵制商业推广！
+// @author       Usuzumi Yukio
+// @match        http://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var observer = new MutationObserver(function () {
+    $('#content_left').find('[data-tuiguang]').parent().parent().text("抵制商业推广！！！");
+    });
+    observer.observe(document.getElementById('wrapper_wrapper'), {childList: true, attributes: true, characterData: true });
+})();
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # promofree
 一起抵制商业推广！
+
+由于Chrome的API限制，开发插件进行响应内容的拦截难度较大，可能要依赖诸如mitmproxy之类的中间人代理，
+因此这一版先做成[Tampermonkey](https://github.com/Tampermonkey/tampermonkey)的用户脚本。
+之后视情况选用其他解决方案。
