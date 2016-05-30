99721094c4dff8747089be9c8d42b6defe347bee
Savor d'Isavano <anohigisavay@gmail.com>
2016-05-31 03:30:52 +0800

    Bugfix and finalize so

--- Promofree.user.js
+++ Promofree.user.js
@@ -18,35 +18,58 @@ var urlMatchers = {
 };
 
 var filters = {
-  baidu: function () {
-    var contentLeftDivs = $('#content_left').find('div');
-    contentLeftDivs.each(function(idx, elem) {
-        if($(elem).find('[data-tuiguang]').length > 0) {
+  baidu: [
+    {
+      filter: function () {
+        var contentLeftDivs = $('#content_left').find('div');
+        contentLeftDivs.each(function(idx, elem) {
+          if($(elem).find('[data-tuiguang]').length > 0) {
             $(elem).text("抵制商业推广");
-        }
-    });
-  },
-  sogou: function () {
-    $('#promotion_adv_container').text("抵制商业推广");
-  },
-  so: function () {
-    $('#e_idea_pp').empty().append($('<li><h3>抵制商业推广</h3></li>'));
-  }
+          }
+        });
+      },
+      observeOn: document.body,
+    }
+  ],
+  sogou: [
+    {
+      filter: function () {
+        $('#promotion_adv_container').text("抵制商业推广");
+      },
+      observeOn: document.body
+    }
+  ],
+  so: [
+    {
+      filter: function () {
+        $('#e_idea_pp').empty().append($('<li><h3>抵制商业推广</h3></li>'));
+      },
+      observeOn: document.getElementById('warper'),
+      observeOptions: {  }
+    }
+  ]
 };
-
 
 
 (function() {
   'use strict';
-  var filter = null;
-  for (var engine in urlMatchers) {
-    var matcher = urlMatchers[engine];
-    if (window.location.href.match(matcher)) {
-      filter = filters[engine];
+  $(document).ready(function () {
+    for (var engine in urlMatchers) {
+      var matcher = urlMatchers[engine];
+      if (window.location.href.match(matcher)) {
+        filters = filters[engine];
+        for(var filterIdx in filters) {
+          var filter = filters[filterIdx];
+          var filterFunc = filter.filter;
+          var observeOn = filter.observeOn || document.body;
+          var observeOptions = Object.assign({}, {childList: true, attributes: true, characterData: true}, filter.observeOptions);
+          if (filterFunc) {
+            var observer = new MutationObserver(filterFunc);
+            observer.observe(observeOn, observeOptions);
+            filterFunc();
+          }
+        }
+      }
     }
-  }
-  if (filter !== null) {
-  var observer = new MutationObserver(filter);
-  observer.observe(document.body, {childList: true, attributes: true, characterData: true });
-  }
+  });
 })();
